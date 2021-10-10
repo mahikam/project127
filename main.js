e89b05f16e9ffc46e3e34a0387f82474b4461765
mahikam <76594479+mahikam@users.noreply.github.com>
2021-10-10 09:20:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+harrypotter="";
+peterpan="";
+function preload(){
+    harrypotter=loadSound("harrypotter.mp3");
+    peterpan=loadSound("peterpan.mp3");
+}
+function setup(){
+    canvas= createCanvas(400,400);
+    canvas.position(477,240);
+
+    video= createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,400,400);
+}
